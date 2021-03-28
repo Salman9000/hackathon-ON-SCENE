a2bfb293a697b632cb82e3573fadfb43a6e0ba82
Salman Hanif <salmanhanif133@gmail.com>
2021-03-28 11:37:56 +0500

    user data

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,91 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NavBar from "./NavBar";
+import { v4 as uuidv4 } from "uuid";
+import UserBlock from "./UserBlock";
 
 function Home() {
+  const [user, setUser] = useState([
+    {
+      id: uuidv4(),
+      firstName: "Salman",
+      lastName: "Hanif",
+      email: "salmanhanif133@gmail.com",
+      password: "salman33",
+      city: "Karachi",
+      country: "Pakistan",
+      vitalSigns: {
+        oxygen: "60",
+        bloodpressure: "60",
+        heartrate: "60",
+        temperature: "60",
+      },
+    },
+    {
+      id: uuidv4(),
+      firstName: "Salman",
+      lastName: "Hanif",
+      email: "salmanhanif133@gmail.com",
+      password: "salman33",
+      city: "Karachi",
+      country: "Pakistan",
+      vitalSigns: {
+        oxygen: "60",
+        bloodpressure: "60",
+        heartrate: "60",
+        temperature: "60",
+      },
+    },
+    {
+      id: uuidv4(),
+      firstName: "Salman",
+      lastName: "Hanif",
+      email: "salmanhanif133@gmail.com",
+      password: "salman33",
+      city: "Karachi",
+      country: "Pakistan",
+      vitalSigns: {
+        oxygen: "60",
+        bloodpressure: "60",
+        heartrate: "60",
+        temperature: "60",
+      },
+    },
+    {
+      id: uuidv4(),
+      firstName: "Salman",
+      lastName: "Hanif",
+      email: "salmanhanif133@gmail.com",
+      password: "salman33",
+      city: "Karachi",
+      country: "Pakistan",
+      vitalSigns: {
+        oxygen: "60",
+        bloodpressure: "60",
+        heartrate: "60",
+        temperature: "60",
+      },
+    },
+  ]);
   return (
     <>
       <NavBar />
+      <div className="bg-gray-200 min-h-screen pt-8">
+        <div className="grid grid-cols-1 gap-1">
+          {user.map((user) => {
+            // if (ad.isApproved == "true") {
+            return (
+              <div className="px-16 flex justify-start" key={user.id}>
+                {/* <Link to={`/ads/${ad._id}`}> */}
+                <UserBlock user={user} />
+                {/* </Link> */}
+              </div>
+            );
+            // }
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/component/UserBlock.js
+++ src/component/UserBlock.js
@@ -1,22 +1,53 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 // import NavBarForm from "./NavBarForm";
-
-function UserBlock() {
+const UserBlock = ({ user, userId }) => {
   return (
-    <>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">VitalDoctor</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Button variant="light">Login</Button>{" "}
-        <Button variant="dark">Signup</Button>{" "}
-      </Navbar>
-    </>
-  );
-}
+    <div className="w-full rounded overflow-hidden shadow-lg bg-white mb-8">
+      <div className="p-4 mb-2">
+        <div className="font-bold text-xl">
+          {user.firstName} {user.lastName}
+        </div>
+        <div className="font-thin mb-2 text-xs">{user.id}</div>
+        <div>
+          <p className="inline-block font-bold text-base pr-4">
+            Oxygen Level:{" "}
+          </p>
+          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {user.vitalSigns.oxygen} %
+          </span>
+        </div>
 
+        <div>
+          <p className="inline-block font-bold text-base pr-4">
+            Blood Pressure:
+          </p>
+          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {user.vitalSigns.bloodpressure} mmHG
+          </span>
+        </div>
+
+        <div>
+          <p className="inline-block font-bold text-base pr-4">Hear rate: </p>
+          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {user.vitalSigns.heartrate} BPM
+          </span>
+        </div>
+
+        <div>
+          <p className="inline-block font-bold text-base pr-4">Temperature: </p>
+          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {user.vitalSigns.temperature} Celcius
+          </span>
+        </div>
+      </div>
+      <div
+        className={`flex pb-4 justify-between text-xs px-4 uppercase text-gray-700`}
+      >
+        <p>{user.country}</p>
+        <p>{user.city}</p>
+      </div>
+    </div>
+  );
+};
 export default UserBlock;
